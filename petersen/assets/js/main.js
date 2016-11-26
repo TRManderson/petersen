e45cd108e04067686fb309f0333f841678ec3ce2
Neil Ashford <ashfordneil0@gmail.com>
2016-11-27 00:30:26 +1000

    added boostrap and made things look nice - also 2/3 of a welcome screen

--- petersen/assets/js/main.js
+++ petersen/assets/js/main.js
@@ -1,90 +1,296 @@
-class NewUser extends React.Component {
-    constructor() {
+let {Button, ControlLabel, FormControl, FormGroup, Nav, NavItem, PageHeader, 
+    Panel} = ReactBootstrap;
+
+class LogIn extends React.Component {
+    /* login window */
+    constructor(props) {
+        /* initialises the window itself, and data within it */
         super();
         this.state = {
-            name: "",
             username: "",
             password: "",
-            passwordConfirm: ""
+            login: props.login
         };
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(event) {
+    handle(event) {
         event.preventDefault();
-
-        if (!this.state.name.match(/^\w* \w*$/)) {
-            alert("Please enter a valid name (i.e. Tom Manderson)");
-            return false;
-        }
-
-        if (!this.state.username.match(/^[a-zA-Z0-9]{3,25}$/)) {
-            alert("Please enter a valid username (3-25 characters containing only alphanumeric characters)");
-            return false;
-        }
-
-        if (this.state.password.length < 4) {
-            alert("Please enter a longer password (minimum 4 characters)");
-            return false;
-        }
-
-        if (this.state.passwordConfirm != this.state.password) {
-            alert("Two passwords are not equal.");
-            return false;
-        }
-
         $.ajax({
-            url: "/user/new",
+            url: "/login",
             data: JSON.stringify({
-                name: this.state.name,
-                username: this.state.username,
+                name: this.state.username,
                 password: this.state.password
             }),
             contentType: "application/json",
             processData: false,
             method: "POST",
-            success: function(data) {
-                console.log(data);
-            }
+            success: function(data) {this.props.login(data);}
         });
     }
 
     render() {
+        /* draw all of the things */
         return (
-            <div id = "new-user">
-            <form onSubmit = {this.handleSubmit}>
-                <label>Name:
-                <input type = "text" value = {this.state.name} onChange = {(event) => {
-                    this.setState({name: event.target.value});
-                }}/>
-                </label>
+            <Panel>
+            <PageHeader>Log In</PageHeader>
+            <form onSubmit = {this.handle.bind(this)}>
 
-                <label>Username:
-                <input type = "text" value = {this.state.username} onChange = {(event) => {
-                    this.setState({username: event.target.value});
-                }}/>
-                </label>
+            <FormGroup
+                controlId = "username"
+                validationState = "success"
+            >
+                <ControlLabel>User Name</ControlLabel>
+                <FormControl
+                    type = "text"
+                    value = {this.state.name}
+                    placeholder = "Your User Name Here"
+                    onChange = {(event) => {
+                        this.setState({username: event.target.value});
+                    }}
+                />
+            </FormGroup>
 
-                <label>Password:
-                <input type = "password" value = {this.state.password} onChange = {(event) => {
-                    this.setState({password: event.target.value});
-                }}/>
-                </label>
+            <FormGroup
+                controlId = "password"
+                validationState = "success"
+            >
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                    type = "password"
+                    value = {this.state.password}
+                    onChange = {(event) => {
+                        this.setState({password: event.target.value});
+                    }}
+                />
+            </FormGroup>
 
-                <label>Confirm Password:
-                <input type = "password" value = {this.state.passwordConfirm} onChange = {(event) => {
-                    this.setState({passwordConfirm: event.target.value});
-                }}/>
-                </label>
-
-                <input type = "submit" value = "Create User"/>
+            <Button
+                type = "submit"
+            >
+                Log In
+            </Button>
             </form>
-            </div>
+            </Panel>
         );
     }
 }
 
+class NewUser extends React.Component {
+    /* new user dialog window */
+    constructor(props) {
+        /* initialises the window itself, and data within it */
+        console.log(props);
+        super();
+        this.state = {
+            name: "",
+            username: "",
+            password: "",
+            passwordConfirm: "",
+            login: props.login
+        };
+    }
+
+    validateName() {
+        /* ensures that the "name" field contains two words */
+        if (!this.state.name.match(/^\w+ \w+$/)) {
+            return "error";
+        }
+        return "success";
+    }
+    validateUserName() {
+        /* ensures that the username field is between 3 and 25 characters long,
+         * containing only alphanumeric characters */
+        if (!this.state.username.match(/^\w{3,25}$/)) {
+            return "error";
+        }
+        return "success";
+    }
+    validatePassword() {
+        /* ensures that the password entered is more than 6 characters long */
+        if (this.state.password.length <= 6) {
+            return "error";
+        }
+        return "success";
+    }
+    validatePasswordConfirm() {
+        /* ensures that the password confirm field matches the main password
+         * field */
+        if (this.state.passwordConfirm != this.state.password ||
+                this.state.passwordConfirm.length <= 6) {
+            return "error";
+        }
+        return "success";
+    }
+
+    handle(event) {
+        /* determines if inputs are valid or not, and calls relevant handler */
+        event.preventDefault();
+        if (this.validateName() == "success" &&
+                this.validateUserName() == "success" &&
+                this.validatePassword() == "success" &&
+                this.validatePasswordConfirm() == "success") {
+            this.handleValid();
+        }
+    }
+    handleValid() {
+        /* makes the relevant API call, and calls parent's login function:
+         * @todo */
+        $.ajax({
+            url: "/user/new",
+            data: JSON.stringify({
+                name: this.state.name,
+                username: this.state.username,
+                password: this.state.username
+            }),
+            contentType: "application/json",
+            processData: false,
+            method: "POST",
+            success: function(data) {this.state.login(data);}
+        });
+    }
+
+    render() {
+        /* draw all of the things */
+        return (
+            <Panel>
+            <PageHeader>Create New User</PageHeader>
+            <form onSubmit = {this.handle.bind(this)}>
+
+            <FormGroup
+                controlId = "name"
+                validationState = {this.validateName()}
+            >
+                <ControlLabel>Name</ControlLabel>
+                <FormControl
+                    type = "text"
+                    value = {this.state.name}
+                    placeholder = "Your Name Here"
+                    onChange = {(event) => {
+                        this.setState({name: event.target.value});
+                    }}
+                />
+            </FormGroup>
+
+            <FormGroup
+                controlId = "username"
+                validationState = {this.validateUserName()}
+            >
+                <ControlLabel>User Name</ControlLabel>
+                <FormControl
+                    type = "text"
+                    value = {this.state.username}
+                    placeholder = "Your User Name Here"
+                    onChange = {(event) => {
+                        this.setState({username: event.target.value});
+                    }}
+                />
+            </FormGroup>
+
+            <FormGroup
+                controlId = "password"
+                validationState = {this.validatePassword()}
+            >
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                    type = "password"
+                    value = {this.state.password}
+                    onChange = {(event) => {
+                        this.setState({password: event.target.value});
+                    }}
+                />
+            </FormGroup>
+
+            <FormGroup
+                controlId = "passwordConfirm"
+                validationState = {this.validatePasswordConfirm()}
+            >
+                <ControlLabel>Confirm Password</ControlLabel>
+                <FormControl
+                    type = "password"
+                    value = {this.state.passwordConfirm}
+                    onChange = {(event) => {
+                        this.setState({passwordConfirm: event.target.value});
+                    }}
+                />
+            </FormGroup>
+
+            <Button
+                type = "submit"
+            >
+                Create New User
+            </Button>
+            </form>
+            </Panel>
+        );
+    }
+}
+
+class Welcome extends React.Component {
+    /* offers the user either a "New User" dialog, or a "Log In" dialog */
+    constructor(props) {
+        /* initialises the window itself, and the data within it */
+        super();
+        this.state = {
+            option: 1
+        };
+    }
+
+    login(data) {
+        console.log(data);
+        if (data.error) {
+            alert(data.error);
+        } else {
+            this.props.login(data);
+        }
+    }
+
+    render() {
+        /* draw all of the things */
+        if (this.state.option == 1) {
+            return (
+                <Panel>
+                <Nav 
+                    bsStyle = "pills"
+                    activeKey = {1}
+                    onSelect = {(select) => {
+                        this.setState({option: select});
+                    }}
+                >
+                    <NavItem eventKey = {1}>Log In</NavItem>
+                    <NavItem eventKey = {2}>New User</NavItem>
+                </Nav>
+
+                <LogIn 
+                    login = {this.login.bind(this)}
+                />
+                </Panel>
+            );
+        } else {
+            return (
+                <Panel>
+                <Nav 
+                    bsStyle = "pills"
+                    activeKey = {2}
+                    onSelect = {(select) => {
+                        this.setState({option: select});
+                    }}
+                >
+                    <NavItem eventKey = {1}>Log In</NavItem>
+                    <NavItem eventKey = {2}>New User</NavItem>
+                </Nav>
+
+                <NewUser
+                    login = {(data) => {
+                        this.login(data);
+                    }}
+                />
+                </Panel>
+            );
+        }
+    }
+}
+
 ReactDOM.render(
-    <NewUser />,
+    <Welcome login = {(data) => {alert(data);}}/>,
     document.getElementById('root')
 );
--- petersen/templates/index.mako
+++ petersen/templates/index.mako
@@ -2,15 +2,20 @@
 <html>
 <head>
     <title>Petersen</title>
-    <link rel="stylesheet/less" type="text/css" href="/assets/css/main.less" >
-    <script src="https://unpkg.com/react@latest/dist/react.js"></script>
-    <script src="https://unpkg.com/react-dom@latest/dist/react-dom.js"></script>
-    <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+    <!-- <link rel="stylesheet/less" type="text/css" href="/assets/css/main.less" > -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js" type="text/javascript"></script>
+
+    <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.4.0/react.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.4.0/react-dom.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react-bootstrap/0.30.7/react-bootstrap.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/flatly/bootstrap.min.css">
+
     <script src="/assets/js/main.js" type="text/babel"></script>
 </head>
 <body>
-<div id="root"></div>
+    <div id = root>
+    </div>
 </body>
 </html>
